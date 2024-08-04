ba520373af2b707aed61ca0e297b9ddafb15a30d
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2024-08-04 16:53:22 -0500

    remove listener and route53 after terraform state rm

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -6,26 +6,7 @@ data "aws_lb_listener" "main_lb_https_listener_data" {
   port              = 443
 }
 
-resource "aws_lb_listener" "main_lb_https_listener" {
-  load_balancer_arn = data.aws_lb.main_lb.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  certificate_arn   = data.aws_acm_certificate.cert.arn
-
-  default_action {
-    type = "fixed-response"
-    fixed_response {
-      status_code  = 200
-      content_type = "text/plain"
-      message_body = "HTTPS listener default action"
-    }
-  }
-
-  tags = local.tags
-}
-
 resource "aws_lb_listener_rule" "forum_client_forward_rule" {
-  # listener_arn = aws_lb_listener.main_lb_https_listener.arn
   listener_arn = data.aws_lb_listener.main_lb_https_listener_data.arn
 
   priority = 3
@@ -42,16 +23,4 @@ resource "aws_lb_listener_rule" "forum_client_forward_rule" {
   }
 
   tags = local.tags
-}
-
-resource "aws_route53_record" "main_lb_dns" {
-  name    = "zzzyx.click"
-  zone_id = "Z0424378XVXHOFVZNJ5G"
-  type    = "A"
-
-  alias {
-    name                   = data.aws_lb.main_lb.dns_name
-    zone_id                = data.aws_lb.main_lb.zone_id
-    evaluate_target_health = true
-  }
 }
