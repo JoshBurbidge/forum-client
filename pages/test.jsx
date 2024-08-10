4b61a871fc55be6419c72f41ebaaadbe19b2a2d8
JoshBurbidge <43246636+JoshBurbidge@users.noreply.github.com>
2024-08-10 09:59:00 -0500

    use state to transform

--- pages/test.jsx
+++ pages/test.jsx
@@ -23,24 +23,13 @@ export default function Test() {
     return () => observer.disconnect();
   }, []);
 
-  // const [transform, setTransform] = useState('transformX(150%)');
-
-  useEffect(() => {
-    if (isIntersecting) {
-      const el = ref.current.querySelector('.child-two');
-      el.classList.add('slide-in');
-    } else {
-      const el = ref.current.querySelector('.child-two');
-      el.classList.remove('slide-in');
-    }
-  }, [isIntersecting]);
 
   return (
     <Box width={'100%'}>
       <Box height={'100vh'}>This is the Header</Box>
       <Box className={'main'} ref={ref} display={'flex'} height={'100px'} bgcolor={'lightgray'} width={'100%'}>
         <Box className="child-one" flex={1}>Child One</Box>
-        <Box className="child-two" flex={1} bgcolor={'gray'}>Child Two</Box>
+        <Box className="child-two" flex={1} bgcolor={'gray'} sx={{transform: isIntersecting ? 'translateX(0)' : 'translateX(150%)' }}>Child Two</Box>
       </Box>
       <Box height={'100vh'}>This is the Footer</Box>
     </Box>
