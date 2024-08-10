2d5c4f4e55e2e2d5ff345d5fc97b81450cb2f52f
Joshua Burbidge <43246636+JoshBurbidge@users.noreply.github.com>
2024-08-10 11:53:52 -0500

    Intersection (#4)
    * working transform intersection example
    
    * use state to transform
    
    * working but missing dependency
    
    * again working but missing dep
    
    * clean up
    
    * add margin
    
    * clean up
    
    * fade in posts
    
    * remove demo pages
    
    * remove import

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -1,32 +1,50 @@
-import { Typography, Link, Card } from "@mui/material";
+import { Typography, Card } from "@mui/material";
 import NextLink from "next/link";
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Post(props) {
   const [hover, setHover] = useState(false);
+  const [isIntersecting, setIsIntersecting] = useState(false);
+
+  const ref = useRef(null);
 
   const toggleHover = function () {
     setHover(!hover);
   };
 
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsIntersecting(entry.isIntersecting);
+      },
+      {
+        rootMargin: '-50px'
+      }
+    );
+    observer.observe(ref.current);
+
+    return () => observer.disconnect();
+  }, []);
+
   return (
     <NextLink href={"/posts/" + props.post.id} passHref>
-      <Link underline={'none'} color={'black'}>
+      <Card onMouseEnter={() => toggleHover()} onMouseLeave={() => toggleHover()}
+        ref={ref}
+        sx={{
+          border: 1,
+          borderColor: hover ? 'black' : 'transparent',
+          transition: 'all ease-in 0.3s;',
+          opacity: isIntersecting ? 1 : 0
+        }}
+      >
+        <Typography pl={1} variant="subtitle2">
+          {`posted by ${props.post.username}`}
+        </Typography>
 
-        <Card onMouseEnter={() => toggleHover()} onMouseLeave={() => toggleHover()}
-          sx={{
-            border: 1,
-            borderColor: hover ? 'black' : 'transparent'
-          }}>
-          <Typography pl={1} variant="subtitle2">
-            posted by {props.post.username}
-          </Typography>
-
-          <Typography p={2} variant='h6'>
-            {props.post.title}
-          </Typography>
-        </Card>
-      </Link>
+        <Typography p={2} variant='h6'>
+          {props.post.title}
+        </Typography>
+      </Card>
     </NextLink >
 
   );
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
-import {  Button, Container, Stack } from '@mui/material';
+import {  Box, Button, Container, Stack } from '@mui/material';
 import PostCard from '../components/PostCard';
 import ArrowButton from '../components/ArrowButton';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import NextLink from "next/link";
 import { getServerDomainForBrowser } from '../utils/request-util';
 
@@ -17,18 +17,55 @@ export async function getServerSideProps() {
   };
 }
 
+const getNextPage = async (setCurrentPosts, offset) => {
+  const res = await fetch(getServerDomainForBrowser() + '/posts?offset=' + offset);
+  const newPosts = await res.json();
+  setCurrentPosts(c => c.concat(newPosts));
+};
+
 export default function Home(props) {
   const [currentPosts, setCurrentPosts] = useState(props.posts);
+  const [isIntersecting, setIsIntersecting] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsIntersecting(entry.isIntersecting);
+      },
+      {
+        rootMargin: '200px'
+      }
+    );
+    observer.observe(ref.current);
+
+    return () => observer.disconnect();
+  }, []);
+
 
   const postsList = currentPosts.map(post => {
     return <PostCard post={post} key={post.id} />;
   });
 
-  async function getNextPage() {
-    const res = await fetch(getServerDomainForBrowser() + '/posts?offset=' + postsList.length);
-    const newPosts = await res.json();
-    return newPosts;
-  }
+
+  useEffect(() => {
+    async function fetchPosts() {
+      if (!loading) {
+        setLoading(true);
+        await getNextPage(setCurrentPosts, postsList.length);
+        setLoading(false);
+      }
+    }
+    if (isIntersecting && !loading) {
+      fetchPosts();
+    }
+  }, [isIntersecting]);
+  // fetches, sets ref back to false, then executes useeffect again
+  // before the render has happened, so it's still intersecting
+  // So, I don't want it to re-execute after posts length changes, I only want
+  // to re-execute on intersection change
 
   return (
     <>
@@ -39,10 +76,14 @@ export default function Home(props) {
 
         <Stack paddingY={3} spacing={3}>
           {postsList}
-          <ArrowButton onClick={async () => {
-            const newposts = await getNextPage();
-            setCurrentPosts(currentPosts.concat(newposts));
-          }} />
+          <ArrowButton/>
+          <Box
+            visibility={'hidden'}
+            height={0}
+            ref={ref}
+          >
+            {'bottom detector'}
+          </Box>
         </Stack>
       </Container>
     </>
